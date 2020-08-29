ea5a0c12bf0ae4ca30e58c07e573fc15c09e3a9c
Zun <zunsamuel97@gmail.com>
2020-08-29 04:08:50 -0500

    step 68

--- demo/src/test/java/com/zun/demo/repository/CourseSpringDataRepositoryTest.java
+++ demo/src/test/java/com/zun/demo/repository/CourseSpringDataRepositoryTest.java
@@ -8,6 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
@@ -38,12 +42,33 @@ class CourseSpringDataRepositoryTest {
 
     @Test
     void playGround(){
-        Course course = new Course("Microservices in 100 steps");
-        repository.save(course);
-        course.setName("Microservices in 100 steps - UPDATED");
-        repository.save(course);
+//        Course course = new Course("Microservices in 100 steps");
+//        repository.save(course);
+//        course.setName("Microservices in 100 steps - UPDATED");
+//        repository.save(course);
         logger.info("Courses -> {}", repository.findAll());
         logger.info("Count -> {}", repository.count());
     }
 
+    @Test
+    void sort(){
+        Sort sort = Sort.by(Sort.Direction.DESC, "name");
+//        Sort sort = Sort.by(Sort.Direction.DESC, "name")
+//                .and(Sort.by(Sort.Direction.ASC, "created_data"));
+        logger.info("Courses -> {}", repository.findAll(sort));
+        logger.info("Count -> {}", repository.count());
+    }
+
+    @Test
+    void pagination(){
+        PageRequest pageRequest = PageRequest.of(0, 3);
+
+        Page<Course> firstPage = repository.findAll(pageRequest);
+        logger.info("First Page -> {}", firstPage);
+        logger.info("First Page Content -> {}", firstPage.getContent());
+
+        Pageable secondPageable = firstPage.nextPageable();
+        Page<Course> secondPage = repository.findAll(secondPageable);
+        logger.info("Second Page Content -> {}", secondPage.getContent());
+    }
 }
